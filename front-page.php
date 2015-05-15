fed86b46ce578b7905e54fa0b022e1c1b24248c7
Victoria Moiseenko <moiseenkovika@gmail.com>
2015-05-15 13:23:03 -0700

    added sidebar. Still working on this

--- front-page.php
+++ front-page.php
@@ -16,10 +16,28 @@
 <div id="wid">
 <div id="widgets">
 <section class="widget-item">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); //start the loop ?>
-<h1><a href="<?php the_permalink(); ?>"><?php the_title(); //get the page or posting title ?></a></h1>
-<?php the_content(''); //get page or posting written content ?>
-<?php endwhile; endif; //end the loop ?>
+<h1>About Us</h1>
+<?php while( have_posts() ): the_post(); //start loop 1?>
+<?php the_content(''); //get the home page content?>
+<?php endwhile; //end loop 1?>
+</section>
+<section class="widget-item">
+<h1>Our Coffee</h1>
+<?php rewind_posts(); //stop loop 1?> 
+<?php query_posts('showposts=4') //give directions to loop 2?>
+<ul>
+<?php while( have_posts() ) : the_post(); ?>
+<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+<?php endwhile; //end loop 2?>
+</ul>
+</section>
+<section class="widget-item">
+<h1>Blog</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p>
+<a href="blog.php">Our Blog</a>
+</section>
+</div>
+</div>
 <small>front-page.php</small>
 </section>
 </div>
--- functions.php
+++ functions.php
@@ -14,4 +14,11 @@ register_nav_menus(array(
 ));
 //
 
+//register sidebar
+register_sidebar(array(
+	'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	'after_widget' => '</div>',
+	'before_title' => '<h2>',
+	'after_title' => '</h2>',
+));
 ?>
